a79dca03a3eb73baaaed0d3817d9d415c19c9045
Denis Stoyanouski <vandenst@tut.by>
2023-05-11 14:47:12 +0300

    added class FileReader

---
+++ src/analyzer/FileReader.java
@@ -0,0 +1,22 @@
+package analyzer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileReader {
+    final String fileName;
+
+    public FileReader(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public byte[] getByteArray() {
+        try (FileInputStream fis = new FileInputStream(String.format("." + File.separator + fileName))) {
+            return fis.readAllBytes();
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+        return new byte[0];
+    }
+}
